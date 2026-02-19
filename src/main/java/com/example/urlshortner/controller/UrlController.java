a9749d1474aab87204fc1376e39c76b06e6f3846
SANTHAN-KUMAR <kiranmayeekarnala@gmail.com>
2026-02-19 21:45:05 +0530

    Update UrlController.java

--- src/main/java/com/example/urlshortner/controller/UrlController.java
+++ src/main/java/com/example/urlshortner/controller/UrlController.java
@@ -28,4 +28,9 @@ public class UrlController {
                 .location(URI.create(originalUrl))
                 .build();
     }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 }
